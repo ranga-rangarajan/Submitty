ca06897d76cc61686be8ba943aad39be99cb6da6
rangaprasad <ranga.rangarajan@spanda.ai>
2024-02-27 10:25:14 +0530

    new file:   build/.cmake/api/v1/query/client-vscode/query.json 	new file:   build/CMakeCache.txt 	new file:   build/CMakeFiles/3.28.2/CMakeCCompiler.cmake 	new file:   build/CMakeFiles/3.28.2/CMakeCXXCompiler.cmake 	new file:   build/CMakeFiles/3.28.2/CMakeDetermineCompilerABI_C.bin 	new file:   build/CMakeFiles/3.28.2/CMakeDetermineCompilerABI_CXX.bin 	new file:   build/CMakeFiles/3.28.2/CMakeSystem.cmake 	new file:   build/CMakeFiles/3.28.2/CompilerIdC/CMakeCCompilerId.c 	new file:   build/CMakeFiles/3.28.2/CompilerIdCXX/CMakeCXXCompilerId.cpp 	new file:   build/CMakeFiles/CMakeConfigureLog.yaml 	new file:   build/CMakeFiles/cmake.check_cache 	new file:   grading/allowed_autograding_commands.cpp 	new file:   "screenshots/Screenshot 2024-02-27 at 10.07.42\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.08.30\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.09.34\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.09.46\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.11.08\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.11.14\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.11.22\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.11.45\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.11.59\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.13.00\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.13.18\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.13.44\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.13.56\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.14.39\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.14.51\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.15.03\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.15.24\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.15.29\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.15.35\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.15.43\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.15.50\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.15.58\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.16.04\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.16.10\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.16.17\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.16.31\342\200\257AM.png" 	new file:   "screenshots/Screenshot 2024-02-27 at 10.17.15\342\200\257AM.png" 	new file:   site/app/models/.Config.php.swp

---
+++ grading/allowed_autograding_commands.cpp
@@ -0,0 +1,4 @@
+#include <string> 
+#include <iostream> 
+extern const std::string GLOBAL_allowed_autograding_commands_default_string = R"()";
+extern const std::string GLOBAL_allowed_autograding_commands_custom_string = R"()";
